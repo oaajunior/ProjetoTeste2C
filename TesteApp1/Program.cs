8e4d9e2982b9e6f14d850265dfa1e46aa97ad974
Oberdan <oaajunior@gmail.com>
2019-03-15 11:35:00 -0300

    Primeira versão do projeto

---
+++ TesteApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TesteApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Digite:");
+            String nomeAlunoLP = Console.ReadLine();
+            Console.WriteLine(nomeAlunoLP);
+            Console.Read();
+        }
+                     
+      
+    }
+}
